f923b4540dd0bff9bdc1063bd66fe70f3b3d6c7d
bellaisgroovy <blockyelephant@gmail.com>
2025-02-05 14:05:58 +0000

    vault backup: 2025-02-05 14:05:57

--- code/more_pthreads_3.c
+++ code/more_pthreads_3.c
@@ -12,31 +12,38 @@ typedef struct _threadinfo {
 } threadinfo_t;
 
 void * getInput(void * arg) {
+    // cast from general void * for meaningful datatype
     threadinfo_t * args = (threadinfo_t *) arg;
+    bool * keepRunning = (bool *) arg;
+
+    int * total = malloc(sizeof(int));
+    *total = 0;
     
-    int currInput;
+    int currInput; // initialise currInput so its not done each iteration
     printf("Input as much as you can --- you have 10 seconds!\n");
-    while (*args->keepRunning) {
+    while (*keepRunning) { // checks value at keeprunning (stored elsewhere is true)
         printf("Add another number!  : ");
-        scanf("%d", &currInput);
-        *args->total += currInput;
+        scanf("%d", &currInput); // gets number input from console
+        *total+= currInput; // adds input to total
     }
-    pthread_exit(NULL);
+    pthread_exit(total);
 }
 
 int main() {
 
     pthread_t thread;
-    bool keepRunning = true;
+    bool keepRunning = true; 
     int total = 0;
-    threadinfo_t args = {&keepRunning, &total};
+    threadinfo_t args = {&keepRunning, &total}; // stores value on this stack to pass to thread
 
-    pthread_create(&thread, NULL, getInput, &args);
+    pthread_create(&thread, NULL, getInput, &keepRunning); // start thread with getInput and arguments from this stack frame
 
-    sleep(10); // sleep for 10 secs
-    keepRunning = false;
-    pthread_join(thread, NULL);
-    printf("\n\nYou managed to enter a total of: %d!\n", total);
+    sleep(3); // sleep for 10 secs
+    keepRunning = false; // set keepRunning which thread has reference to false so it stops
+
+    int * total2;
+    pthread_join(thread, (void *) &total2); 
+    printf("\n\nYou managed to enter a total of: %d!\n", *total2);
 
     pthread_exit(NULL);
 
