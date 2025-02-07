8d94d1536300a899ed69d4ba760988b5b411d8cb
bellaisgroovy <blockyelephant@gmail.com>
2025-02-07 15:44:29 +0000

    vault backup: 2025-02-07 15:44:29

--- code/dot_product/dotprod.c
+++ code/dot_product/dotprod.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ARRAYSIZE 100000
-#define NUMTHREADS 4
+#define ARRAYSIZE 1000000
+#define NUMTHREADS 100
 #define RANDLIMIT 50 // biggest number allowed from random gen ( +1 )
 #define RANDINT() rand()%RANDLIMIT // this will work for us!
 
@@ -19,20 +19,12 @@ void * dotProd(void * arg) {
     // Q2: introduce locking to avoid a race condition, _inside_ the for loop
     chunk_t * toCompute = (chunk_t *)arg;
 
-    int chunkResult = 0;
-
     int index, multAtIndex;
     for (index = toCompute->start; index < toCompute->end; index++) {
         // compute the dot product, one index at a time!
         multAtIndex = (toCompute->firstArr[index] * toCompute->secondArr[index]);
-        chunkResult = chunkResult + multAtIndex;
+        *toCompute->result = *toCompute->result + multAtIndex;
     }
-
-    pthread_mutex_lock(toCompute->resultMutex);
-
-    *toCompute->result = *toCompute->result + chunkResult;
-
-    pthread_mutex_unlock(toCompute->resultMutex);
 
     pthread_exit(NULL);
 }
@@ -59,35 +51,39 @@ int main() {
     // produce a sensible argument array
     pthread_mutex_t resultMutex; // a mutex to write the result
     pthread_mutex_init(&resultMutex, NULL); // initialise the mutex
+
     chunk_t args[NUMTHREADS]; // NUMTHREADS arguments, one for each of the threads we will start
-    long double calculationResult = 0; // to hold the result of our calculations
+
+    long double calculationResult[NUMTHREADS]; // to hold the result of our calculations
+    
     int chunk_size = ARRAYSIZE/NUMTHREADS; // How much of the array should each chunk calculate on?
     for (i = 0; i < NUMTHREADS; i++) {
         args[i].firstArr = arr1;
         args[i].secondArr = arr2;
         args[i].start = i*chunk_size;
         args[i].end = (i+1)*chunk_size;
-        args[i].result = &calculationResult;
+
+        calculationResult[i] = 0;
+        args[i].result = &calculationResult[i];
+
         args[i].resultMutex = &resultMutex;
         printArgs(args[i]);
     }
     
-    // Q1: note, you will need some pthread_ts for your call to pthread_create...
-    // You probably want to initialise those here.
     pthread_t threads[NUMTHREADS];
 
     for (i = 0; i < NUMTHREADS; i++) {
-        // Q1: start the threads here!
-        // The i-th thread should take a pointer to args[i] as an argument.
         pthread_create(&threads[i], NULL, dotProd, (void *) &args[i]);
     }
 
+    long double total = 0;
     for (i = 0; i < NUMTHREADS; i++) {
-        // Q1: join your threads from above here!
         pthread_join(threads[i], NULL);
+
+        total += calculationResult[i];
     }
 
-    printf("The dot product for our random arrays works out as %Lf.\n", calculationResult);
+    printf("The dot product for our random arrays works out as %Lf.\n", total);
 
     return 0;
 
