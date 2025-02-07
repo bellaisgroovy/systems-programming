7e0208c89868fc2140d0ce5f6711694fdccc09bd
bellaisgroovy <blockyelephant@gmail.com>
2025-02-07 10:11:02 +0000

    vault backup: 2025-02-07 10:11:02

--- code/more_pthreads_5.c
+++ code/more_pthreads_5.c
@@ -1,4 +1,8 @@
 #include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // the rest is up to you! Implement a program which starts _two_ pthreads.
 // One reads input from the user, and adds numbers to a total.
@@ -7,3 +11,51 @@
 // to both threads that they should join, just like in 3.c.
 // You can use sleep(int seconds) from unistd.h/windows.h to make your
 // threads sleep.
+
+typedef struct _threadinfo {
+    bool * keepRunning;
+    int * total;
+} threadinfo_t;
+
+void * addInputs(void * arg) {
+    threadinfo_t * args = (threadinfo_t *) arg;
+
+    int currentInput = 0;
+
+    while(*args->keepRunning) {
+        printf("Add another number!  : ");
+        scanf("%d", &currentInput);
+        *args->total += currentInput;
+    }
+
+    pthread_exit(NULL);
+}
+
+void * printTotal(void * arg) {
+    threadinfo_t * args = (threadinfo_t *) arg;
+
+    while(*args->keepRunning) {
+        printf("total: %i\n", *args->total);
+        sleep(3);
+    }
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t adder;
+    pthread_t printer;
+
+    bool keepRunning = true;
+    int total = 0;
+    threadinfo_t arg = {&keepRunning, &total};
+
+    adder = pthread_create(&adder, NULL, addInputs, &arg);
+    printer = pthread_create(&adder, NULL, printTotal, &arg);
+
+    sleep(10);
+
+    keepRunning = false;
+
+    pthread_exit(NULL);
+}
